35b6088ad82214ebfd78ca0e9dae04370fef1cf1
uyaji <yuichiro.uyama@gmail.com>
2017-03-02 14:26:43 +0900

    implement datepicker

--- src/main/scala/code/snippet/BandView.scala
+++ src/main/scala/code/snippet/BandView.scala
@@ -9,9 +9,11 @@ import net.liftweb.http._
 import S._
 import SHtml._
 import net.liftweb.http.js.{JsCmd, JsCmds}
+import net.liftweb.http.js.JsCmds.Run
 import code.model._
 import code.logic._
 import java.util.Date
+import java.text.SimpleDateFormat
 
 class BandView {
   val bandid = Util.paramGet("bandid")
@@ -24,8 +26,8 @@ class BandView {
                   case "0" => new BandSeq(new Date(0), new Date(0), seq.toInt)
                   case _   => band.bandSeqs.filter{ bs => bs.seq == seq.toInt }.head
                 }
-  var startat = Util.dateToString(bandSeq.bandSeqStartAt.get)
-  var endat = Util.dateToString(bandSeq.bandSeqEndAt.get)
+  var startat = bandSeq.bandSeqStartAt.get
+  var endat = bandSeq.bandSeqEndAt.get
 
   def list(html: NodeSeq): NodeSeq = {
     def renderRow(): NodeSeq = {
@@ -42,15 +44,28 @@ class BandView {
   
   def add(form: NodeSeq): NodeSeq = {
     def doBind(form: NodeSeq): NodeSeq = {
+      val defaultst = (dateFormat format new Date(0))
+      S.appendJs(enhance)
       var sel =
         "name=seq" #> SHtml.text( seq, seq = _, "readonly" -> "readonly") &
-        "name=startat" #> SHtml.text( startat, startat = _) &
-        "name=endat" #> SHtml.text( endat, endat = _) &
+        "name=startat" #> SHtml.text( dateFormat format startat, logDateStart) &
+        "name=endat" #> SHtml.text( dateFormat format endat, logDateEnd) &
         "type=submit" #> SHtml.onSubmitUnit(registerSeq);
       return sel(form)
     }
     doBind(form)
   }
+  
+  def logDateStart(s: String): Unit = {
+    startat = tryo(dateFormat parse s) getOrElse new Date(0)
+  }
+  def logDateEnd(s: String): Unit = {
+    endat = tryo(dateFormat parse s) getOrElse new Date(0)
+  }
+  
+  val dateFormat = new SimpleDateFormat("yyyy")
+
+  val enhance = Run("$('#startat').datepicker({dateFormat: 'yy', changeYear: true}); $('#endat').datepicker({dateFormat: 'yy', changeYear: true});")
 
   def registerSeq() {
     val msg = "Can not register.Already exsist Band Seq. Please update"
@@ -58,11 +73,11 @@ class BandView {
     val process = Logic.select(duplicateSeqCheck, _==_ )(bandid.toLong, initSeq.toLong, initSeq.toLong, msg, path)
     process match {
       case "add" => {
-        val msg = Process.add((f: (Target, Binder) => Boolean) => (Target, Relation, Binder, String) => Nil, (Target, Binder) => true, String => Nil, "", null, new BandSeq(Util.stringToDate(startat), Util.stringToDate(endat), seq.toInt).band(band.id.get), null, None, 0, "added seq " + seq, "")
+        val msg = Process.add((f: (Target, Binder) => Boolean) => (Target, Relation, Binder, String) => Nil, (Target, Binder) => true, String => Nil, "", null, new BandSeq(startat, endat, seq.toInt).band(band.id.get), null, None, 0, "added seq " + seq, "")
         S.error(msg)
       }
       case "update" => {
-        val msg = Process.update((f2: (Long, Long) => Target, f3: Long => Binder,  f4: String => List[Target]) => (a, b, c, d) => new Result(Nil, ""), (a, b) => band.bandSeqs.filter{ bs => bs.seq == seq.toLong }.head.bandSeqStartAt(Util.stringToDate(startat)).bandSeqEndAt(Util.stringToDate(endat)), a => null, c => Nil, upload => false, c => Nil, "", 0L, null, band, 0L, None, "updated seq " + seq, "", "")
+        val msg = Process.update((f2: (Long, Long) => Target, f3: Long => Binder,  f4: String => List[Target]) => (a, b, c, d) => new Result(Nil, ""), (a, b) => band.bandSeqs.filter{ bs => bs.seq == seq.toLong }.head.bandSeqStartAt(startat).bandSeqEndAt(endat), a => null, c => Nil, upload => false, c => Nil, "", 0L, null, band, 0L, None, "updated seq " + seq, "", "")
         S.error(msg)
       }
     }
