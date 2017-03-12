0fa1f946e5227e5d51ab69ffde2ecbbbe201f128
uyaji <yuichiro.uyama@gmail.com>
2017-03-12 09:31:19 +0900

    refacoring

--- src/main/scala/code/lib/Suggest.scala
+++ src/main/scala/code/lib/Suggest.scala
@@ -3,6 +3,7 @@ package code.lib
 import scala.collection.mutable._
 import scala.xml.NodeSeq
 import net.liftweb.util.Helpers._
+import net.liftweb.mapper._
 import net.liftweb.common._
 import net.liftweb.http._
 import net.liftweb.json._
@@ -13,7 +14,7 @@ object Suggest {
   def suggestion(key: String, fieldName: String): Box[LiftResponse] = {
     implicit val formats = DefaultFormats
     val titles = fieldName match {
-      case "searchAlbumtitle" =>Album.findAll().map(a => a.albumtitle.get).toList
+      case "searchAlbumtitle" =>Album.findAll(OrderBy(Album.albumtitle, Ascending)).map(a => a.albumtitle.get).toList
       case "searchArtist" | "artistname" => {
         val newTitles = new ListBuffer[BandJson]()
         for(band <- Band.findAll().filter(_.bandname.get.toLowerCase.startsWith(key.toLowerCase))) {
@@ -22,12 +23,12 @@ object Suggest {
         }
           newTitles.sortWith(_.label < _.label)
       }
-      case "searchTrack" =>Track.findAll().map(t=> t.tracktitle.get).toList
-      case "searchPlayer" | "membername" =>Player.findAll().map(p=> p.name.get).toList
+      case "searchTrack" =>Track.findAll(OrderBy(Track.tracktitle, Ascending)).map(t=> t.tracktitle.get).toList
+      case "searchPlayer" | "membername" =>Player.findAll(OrderBy(Player.name, Ascending)).map(p=> p.name.get).toList
       case _ => List()
     }
     val json = fieldName match {
-      case "searchArtist" | "artistname" => parse(Serialization.write(titles.asInstanceOf[ListBuffer[BandJson]]))
+      case "searchArtist" | "artistname" => parse(Serialization.write(titles))
       case _ => Extraction.decompose(titles.asInstanceOf[List[String]].filter(_.toLowerCase.startsWith(key.toLowerCase)))
     }
     val headerContentType = "Content-Type"
